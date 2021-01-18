25a389b9ff168803a75a8522de750e7d1cf4cc2f
Het Patel <75133701+HET-CE@users.noreply.github.com>
2021-01-18 18:02:40 +0530

    update mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // MONGODB_URL='mongodb://127.0.0.1:27017/task-manager-api'
 // mongoose.connect(process.env.MONGODB_URL, {
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb+srv://TaskManager:iM29siRQ@cluster0.le1e2.mongodb.net/task-manager-api?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
